ce0f74720dd099d32523572897503dc1e2e9bcf3
tovarm-uchi <56130057+tovarm-uchi@users.noreply.github.com>
2020-05-17 15:10:52 -0500

    my 10print.js with the 'alt syntax'

--- 10print.js
+++ 10print.js
@@ -2,19 +2,34 @@
   one possible implemenation of the 10print.org algorithm written in JavaScript
 */
 
-const w = process.stdout.columns
+let sw = process.stdout.columns - 1
+if ( sw % 2 != 0) sw = sw - 1
+
+const p = ['═','║','╔', '╗', '╚', '╝', '╠', '╣','╦','╩','╬']
+const c = ['╭','╮','╯','╰',]
+
+const rn = max => Math.floor(Math.random()*max)
+
+const bright = [100, 40, 30, 90]
+const v = rn(8)
+const b = Math.round(Math.random())
+const pc = `\x1b[${bright[b] + v}m\x1b[${bright[b + 2] + v}m`
+const cc = `\x1b[${bright[(b+3) % 4] + v}m\x1b[${bright[(b+5) % 4]  + v}m`
+
+let d = 2; //delta aka how many new characters to print per line
+let j = 2;
 
 function draw () {
   setTimeout(draw, 1000)
-  let output = ''
-  for (let i = 0; i < w; i++) {
-    if (Math.random() > 0.5) {
-      output += '\\'
-    } else {
-      output += '/'
-    }
+  let out = ''
+  if (j > sw || j < 2) d = -d
+  j += d
+
+  for(let i = 0; i < sw ; i++){
+      if(i > (sw-j)/2 && i < ((sw-j)/2 + j)) out += pc + p[rn(p.length)]
+      else out += cc + c[rn(c.length)]
   }
-  console.log(output)
+  console.log(out)
 }
 
 draw()
